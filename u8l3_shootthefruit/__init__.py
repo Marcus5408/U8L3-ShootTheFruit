8395c529483edba03f5a6f75e8790118b2611f90
Marcus Jones <marcusjones8119@gmail.com>
2024-03-04 13:06:43 -0500

    Refactor game loop and scoring logic

--- u8l3_shootthefruit/__init__.py
+++ u8l3_shootthefruit/__init__.py
@@ -23,22 +23,14 @@ g = 0
 b = 100
 
 
-# render the text for later
-message = "Click the fruit to score!"
-display_message = my_font.render(message, True, (255, 255, 255))
-
-
 # Instantiate the apple
 a = Apple(40, 40)
 
-
 # The loop will carry on until the user exits the game (e.g. clicks the close button).
-run = True
-
-
+score = 0
+run, game_end = True, False
 # -------- Main Program Loop -----------
 while run:
-
     # --- Main event loop
     ## ----- NO BLIT ZONE START ----- ##
     for event in pygame.event.get():  # User did something
@@ -46,13 +38,37 @@ while run:
             run = False
         if event.type == pygame.MOUSEBUTTONUP:
             if a.rect.collidepoint(event.pos):
-                a.move(a.x + 3, a.y + 3)
+                if a.image.get_at((event.pos[0] - a.rect.x, event.pos[1] - a.rect.y))[3] != 0:
+                    a.move(
+                        random.randint(0, SCREEN_WIDTH - a.image_size[0]),
+                        random.randint(0, SCREEN_HEIGHT - a.image_size[1]),
+                    )
+                    score = score + 1 if score < 11 else score
+                    game_end = True if score == 10 else False
     ##  ----- NO BLIT ZONE END  ----- ##
 
     ## FILL SCREEN, and BLIT here ##
-    screen.fill((r, g, b))
-    screen.blit(display_message, (0, 0))
-    screen.blit(a.image, a.rect)
+    if not game_end:
+        screen.fill((r, g, b))
+        message = f"Click the fruit to score! Current score: {score}"
+        display_message = my_font.render(message, True, (255, 255, 255))
+        screen.blit(display_message, (0, 0))
+        screen.blit(a.image, a.rect)
+    else:
+        screen.fill((r, g, b))
+        message = f"Current score: {score}"
+        display_message = my_font.render(message, True, (255, 255, 255))
+        screen.blit(display_message, (0, 0))
+        message = f"Thanks for playing!"
+        display_message = my_font.render(message, True, (255, 255, 255))
+        screen.blit(
+            display_message,
+            (
+                SCREEN_WIDTH // 2 - display_message.get_width() // 2,
+                SCREEN_HEIGHT // 2 - display_message.get_height() // 2,
+            ),
+        )
+
     pygame.display.update()
     ## END OF WHILE LOOP
 
