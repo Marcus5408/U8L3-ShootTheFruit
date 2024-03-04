10c9a556f29091d870f51a1f595b06083b039dec
Marcus Jones <marcusjones8119@gmail.com>
2024-03-04 10:54:11 -0500

    Update image path for Apple class

--- u8l3_shootthefruit/apple.py
+++ u8l3_shootthefruit/apple.py
@@ -9,7 +9,7 @@ class Apple:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.image = pygame.image.load("apple.png")
+        self.image = pygame.image.load("u8l3_shootthefruit/apple.png")
         self.image_size = self.image.get_size()
         self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
 
